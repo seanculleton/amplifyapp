475c6274929a0cf3b442a6bbda6109aae01f0321
seanculleton <sean@donedeal.ie>
2020-12-21 17:08:40 +0000

    add suggested dealer showrooms

--- src/App.js
+++ src/App.js
@@ -23,9 +23,6 @@ function App() {
   }
 
   const outputHTMLSuggestions = (matches, searchText) => {
-    if (searchText.length > 2) {
-      lookupDealerDirectory(searchText)
-    }
 
     if (matches.length > 0) {
       let html = '';
@@ -58,7 +55,10 @@ function App() {
           match.newCars.forEach(newCar => {
             html += newCarAdComponent(match, newCar)
           })
-          
+        }
+
+        if (match.type === 'dealer') {
+          html += suggestDealerComponent(match);
         }
       });
 
@@ -68,27 +68,17 @@ function App() {
     }
   }
 
-  const lookupDealerDirectory = async searchText => {
-    let response = await fetch("https://www.donedeal.ie/search/api/v4/dealers/", {
-      "headers": {
-        "accept": "application/json, text/plain, */*",
-        "Access-Control-Allow-Origin": "https://master.d2iifvfickjnso.amplifyapp.com/",
-        "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
-        "content-type": "application/json;charset=UTF-8",
-        "platform": "web",
-        "sec-fetch-dest": "empty",
-        "sec-fetch-mode": "cors",
-        "sec-fetch-site": "same-origin",
-        "cookie": "JSESSIONID=4E790F8D6E4D3ADCAF4B5D0716F57366; xtidc=191119104539573729; ddSearchViewPreferenceV2=list; _hjDonePolls=478269%2C483332; NPS_4fb20f86_surveyed=true; __stripe_mid=ee7ddfdf-8546-4dd6-b31d-3004f86e33fb377a12; ab.storage.deviceId.dde8ab7a-7fcb-4fdd-b013-6854facbea7e=%7B%22g%22%3A%22198f3013-9c88-4a3e-8b18-d01c8b978f38%22%2C%22c%22%3A1602164969547%2C%22l%22%3A1602164969547%7D; _ga=GA1.2.1176357775.1602164974; donedealCC=\"{\\\"version\\\":1,\\\"createDate\\\":1602164976909,\\\"lastUpdated\\\":1602164976909,\\\"expires\\\":1617889776909,\\\"strictlyNecessary\\\":true,\\\"analytics\\\":true,\\\"functional\\\":true,\\\"advertising\\\":true}\"; _hjid=9272aab2-7767-4967-82ef-9e5651947edf; __zlcmid=10djVIPngF7oxSy; __utma=220414087.1176357775.1602164974.1602782682.1602782682.1; __utmz=220414087.1602782682.1.1.utmcsr=braze-quality-test.donedeal.ninja|utmccn=(referral)|utmcmd=referral|utmcct=/; _hjMinimizedPolls=619145; _fbp=fb.1.1605696196929.1079815824; _gcl_au=1.1.1738394294.1605988494; _gaexp=GAX1.2.qP8WMzxCQSiTgXHtlCD4vA.18690.1; cto_bundle=RseYYF9SQzRBZ25sVG1UR29La0l1JTJCdk5YMU54S2RielNiTSUyRnprZ1gwSk9kaHlzJTJGTldBQkRoZSUyRkQ3bTNDJTJGSmV4NHN1ZGp3R3M1N29IN2kwVklBOXoxTXRaanQlMkZmNVR0VUJTZXRTTDBOTEdmYlltRjNaMWQ1TzNTOEs2U0tJMktFazc3c1E1QlliQlhpcXNmaDZvVlcxSW1YWlElM0QlM0Q; _hjTLDTest=1; ddSiteFocus=motor; uuid=A06C5471-5057-4E0E-8A76-D0A5678AC336; xtvrn=$557889$501695$; xtan=1-1015816; xtant=1; ab.storage.userId.dde8ab7a-7fcb-4fdd-b013-6854facbea7e=%7B%22g%22%3A%222099809%22%2C%22c%22%3A1608198610027%2C%22l%22%3A1608198610027%7D; NPS_4fb20f86_last_seen=1608551797227; _gid=GA1.2.1972965063.1608551799; _hjIncludedInSessionSample=1; _hjAbsoluteSessionInProgress=0; ab.storage.sessionId.dde8ab7a-7fcb-4fdd-b013-6854facbea7e=%7B%22g%22%3A%22056f71c1-2579-62ec-54af-ece9ef1f173d%22%2C%22e%22%3A1608561664406%2C%22c%22%3A1608559858401%2C%22l%22%3A1608559864406%7D; DDSEISIUN=A78FEBEE11FCA204500B6C3B823BDF3A; DDSearchStartTime=1608566733577; DDLastViewedAd=26825831; __gads=ID=d5d79ae3444309d6-22b518fa8fa6003c:T=1608566778:RT=1608566778:S=ALNI_MZSh_PcTMmVxE49_bXl5S78NSt1jg; utag_main=v_id:01750879706c0017a1ff6c934eeb03078005807000ac2$_sn:95$_ss:0$_st:1608568621395$_pn:52%3Bexp-session$ses_id:1608559858580%3Bexp-session"
-      },
-      "referrer": "https://www.donedeal.ie/find-a-dealer?terms=bolands",
-      "referrerPolicy": "strict-origin-when-cross-origin",
-      "body": "{\"terms\":\"boland\"}",
-      "method": "POST",
-      "mode": "cors"
-    });
-    console.log(response);
+  const suggestDealerComponent = match => {
+    return (`
+      <div className="card card-body mb-1">
+          <a href="https://www.donedeal.ie/dealer/${match.value}">
+            <h4>View the ${match.keyword} showroom</h4>
+          </a>
+        </div>
+      `
+    );
   }
+
   const newCarAdComponent = (match, newCar) => {
     return (`
       <div className="card card-body mb-1">
@@ -163,6 +153,7 @@ function App() {
             <li>Suggest filtered search in Dealer Directory</li>
             <li>Suggest top level sections. Tip! try typing Cars, Campers or Tractors</li>
             <li>Suggest New Car ad details. Tip! try typing Mercedez-Benz</li>
+            <li>Suggest Dealer Showrooms. Tip! try typing Boland</li>
           </ul>
         </div>
       </div>
